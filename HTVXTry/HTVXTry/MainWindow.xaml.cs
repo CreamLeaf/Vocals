c2e5b880266e040e7448e4bda653a7cb2637cd9a
CreamLeaf <71951376+CreamLeaf@users.noreply.github.com>
2025-10-05 09:20:13 -0400

    Hack The Valley X first try

---
+++ HTVXTry/HTVXTry/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System.Speech.Synthesis.TtsEngine;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HTVXTry
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Microphone.MakeMenuGrammar();
+        }
+
+        // Click function to start
+        private void StartClick(object sender, RoutedEventArgs e)
+        {
+            Start();
+        }
+
+        // Activate the microphone if AlwaysMic is false
+        private void MicButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Microphone.AlwaysMic) { return; }
+
+            Microphone.Mic(CommandText);
+            switch (Microphone.Word)
+            {
+                case "Start":
+                    Start();
+                    break;
+                case "Begin":
+                    Start();
+                    break;
+                case "Starrrt":
+                    Start();
+                    break;
+                case "Sssstarrrt":
+                    Start();
+                    break;
+                case "How":
+                    HowToPlay();
+                    break;
+                case "HTP":
+                    HowToPlay();
+                    break;
+                case "Credits":
+                    Credits();
+                    break;
+                case "null":
+                    DisplayAudioError();
+                    break;
+                default:
+                    DisplayAudioError();
+                    break;
+            }
+        }
+
+        // Start the project
+        public void Start()
+        {
+            CommandText.Text = "Start!";
+        }
+
+        // Menu methods
+        public void HowToPlay()
+        {
+            CommandText.Text = "HTP";
+        }
+
+        public void Credits()
+        {
+            CommandText.Text = "ERRO";
+        }
+
+        public void End()
+        {
+
+        }
+
+        // Error method for audio
+        public void DisplayAudioError()
+        {
+            CommandText.Text = "Error processing your audio. Try again.";
+        }
+    }
+}
---
+++ HTVXTry/HTVXTry/Microphone.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Diagnostics;
+
+namespace HTVXTry
+{
+    public class Microphone
+    {
+        public static SpeechRecognizer speechRecognizer = new SpeechRecognizer();
+        //static SpeechRecognitionEngine Recognition = new SpeechRecognitionEngine();
+        static SpeechRecognitionEngine Rec = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
+        static Grammar Gramm;
+        public static string Word = " ";
+        public static string[] VariationWords;
+        public TextBox RecText;
+
+        public static bool AlwaysMic = false; // Determine if the microphone should always be on.
+
+        public Microphone()
+        {
+            Gramm = new Grammar(new GrammarBuilder());
+        }
+
+        // Got help from Microsoft .NET documentation
+        public static void MakeMenuGrammar()
+        {
+            // Make the variations of acceptable words
+            VariationWords = new string[2]{ "Starrrt", "Sssstarrrt" };
+
+            GrammarBuilder builder = new GrammarBuilder();
+            builder.Append("Start");
+            builder.Append("Begin");
+            builder.Append("How");
+            builder.Append("Credits");
+            builder.Append("End");
+
+            
+
+            Gramm = new Grammar(builder);
+            //Recognition.LoadGrammar(Gramm);
+            Rec.LoadGrammar(Gramm);
+
+            //Recognition.SpeechDetected += Recognition_SpeechDetected;
+            //Recognition.SpeechRecognized += Recognition_SpeechRecognized;
+            Rec.SpeechRecognized += Recognition_SpeechRecognized;
+        }
+
+        private static void Recognition_SpeechRecognized(object? sender, SpeechRecognizedEventArgs e)
+        {
+            Word = e.Result.Text;
+        }
+
+        private static void Recognition_SpeechDetected(object? sender, SpeechDetectedEventArgs e)
+        {
+            //Recognition.EmulateRecognize("Credits");
+     
+        }
+
+        // Use the microphone to detect audio
+        public static string Mic(TextBox Message)
+        {
+            //using (Recognition = new SpeechRecognitionEngine(SpeechRecognitionEngine.InstalledRecognizers()[0]))
+            
+            {
+                Message.Text = "Startebebe";
+                Console.WriteLine("\nStartebebebeb");
+                Thread.Sleep(2000);
+                Message.Text = "RE";
+
+                //Recognition = new SpeechRecognitionEngine();
+                //Recognition.SetInputToDefaultAudioDevice();
+                //Recognition.LoadGrammar(Gramm);
+                //RecognitionResult Result = Recognition.Recognize(TimeSpan.FromSeconds(3.0));
+                //RecognitionResult Result = Recognition.EmulateRecognize("Begin");
+
+                Rec.SetInputToDefaultAudioDevice();
+                RecognitionResult ResultRec = Rec.Recognize(TimeSpan.FromSeconds(3.5));
+
+                // Try to get the word from the RecognitionResult
+                //if (ResultRec != null)
+                //{
+                //    Word = ResultRec.Text;
+                //    Message.Text = Word;
+                //}
+                //else
+                //{
+                //    Message.FontSize = 50.0;
+                //    Word = "null";
+                //}
+            }
+            return Word;
+        }
+    }
+}
